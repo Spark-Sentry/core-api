9958b5b795bddf765ff493d816986655f0c3f171
Jibe0123 <Jbagostin@gmail.com>
2024-05-19 01:05:47 +0200

    Adapt db conn

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module core-api
 go 1.22
 
 require (
+	github.com/aws/aws-sdk-go v1.53.5
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.5.1
@@ -23,6 +24,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- internal/infrastructure/database/database.go
+++ internal/infrastructure/database/database.go
@@ -3,6 +3,9 @@ package database
 import (
 	"core-api/internal/domain/entities"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
@@ -50,11 +53,25 @@ func InitDB() {
 	}
 
 	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
+	awsRegion := os.Getenv("AWS_REGION")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
+	// Create a new AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create AWS session: %v", err)
+	}
+
+	// Generate an IAM auth token
+	authToken, err := rdsutils.BuildAuthToken(fmt.Sprintf("%s:3306", dbHost), awsRegion, dbUser, sess.Config.Credentials)
+	if err != nil {
+		log.Fatalf("Failed to generate IAM auth token: %v", err)
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, authToken, dbHost, dbName)
 	fmt.Println("Connecting with DSN:", dsn)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
